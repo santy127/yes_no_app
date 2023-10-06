5965caa6aba5b4eb608887d5ae5823fb91c11945
Practicante.ti4 <practicante.ti4@austinreed.com.co>
2023-10-06 15:47:22 -0500

    First preview of the messaging chat app yes no app

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/32322/square_thumb%402x.png'),
+          ),
+        ),
+        title: const Text('Mi amor ❤'),
+        centerTitle: false,  // Para centrar forzadamente el titulo 
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final chatProvider = context.watch<ChatProvider>();
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric( horizontal: 10 ),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: chatProvider.messageList.length,
+                itemBuilder: (context, index) {
+                  final message = chatProvider.messageList[index];
+
+                  return ( message.fromWho == FromWho.hers )
+                  ? const HerMessageBubble()
+                  : const MyMessageBubble();
+              }) 
+            ),
+
+            // Caja de texto de mensajes
+            const MessageFieldBox(),
+          ], 
+        ),
+      ),
+    );
+  }
+}
